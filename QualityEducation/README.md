a3608e0899f560fd8356e1eda18816412f9136eb
Adarsh-Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-03-31 15:58:43 +0530

    Add files via upload
    Uploaded final files of the website

---
+++ QualityEducation/README.md
@@ -0,0 +1,8 @@
+# PreFinalProjectGoogleSolution  
+
+
+Thanks to Bootstrap , youtube tutorials ,  DSC team for workshop and every single friend who helped me weather directly or indirectly .
+Last but not least Thanks to me and my team for our hard work without us this woudl have been only a dream .
+
+
+                      --- ADARSH SAURABH and TEAM
---
+++ QualityEducation/static/js/index.js
@@ -0,0 +1,51 @@
+// console.log('We are going to rock this.');
+
+// $(function () {
+//     console.log("This is also working");
+
+
+//     // let isVisible = false;
+//     // let login_form = true;
+
+//     // $('.log_auth').click(function () {
+//     //     $('.auth_window').css('transform', 'scale(1)');
+//     //     login_form = true;
+//     //     toggleForm();
+//     //     isVisible = true;
+//     // })
+
+//     // $('.remove').click(() => {
+//     //     $('.auth_window').css('transform', 'scale(0)');
+//     //     isVisible = false;
+//     // })
+
+//     // $('.sign_auth').click(function () {
+//     //     $('.auth_window').css('transform', 'scale(1)');
+//     //     login_form = false;
+//     //     toggleForm();
+//     //     isVisible = true;
+//     // })
+
+
+//     // $('.login_switch').click(function () {
+//     //     login_form = true;
+//     //     toggleForm();
+//     // })
+//     // $('.signup_switch').click(function () {
+//     //     login_form = false;
+//     //     toggleForm();
+//     // })
+
+//     // function toggleForm() {
+//     //     if (login_form) {
+//     //         $('#login_form').css('display', 'flex');
+//     //         $('#signup_form').css('display', 'none');
+//     //         login_form = false;
+//     //     }
+//     //     else {
+//     //         $('#login_form').css('display', 'none');
+//     //         $('#signup_form').css('display', 'flex');
+//     //         login_form = true;
+//     //     }
+//     // }
+// })
